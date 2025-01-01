cf3bd531a2e98d055d8a075f47ce6524645d0400
Nayan <nayanthakre379@gmail.com>
2025-01-01 20:26:17 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3); // we are using node js not browser
+
+let name = "Nayan"
+let age = 24
+let isLoggedIn = false
+
+// number => 2 to the power of 53
+// bigint for big number 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => mtlb abhi value assing nh hue hai 
+// symbol => use to find the uniqueness
+
+
+
+// object
+
+console.log(typeof "Nayan")// string
+console.log(typeof null)//object
+console.log(typeof undefined)// undefined
